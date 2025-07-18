ac35e791f24795afa910851129959e44ca853bc3
Jana-Ruth <ruthewenode73@gmail.com>
2025-07-18 17:14:42 +0100

    remove ?

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -36,7 +36,7 @@ const Users: React.FC<UsersPageProps> = ({ posts }) => {
         )}
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {posts?.map(user => (
+          {posts.map(user => (
             <UserCard key={user.id} {...user} />
           ))}
            
